8911b76278056623e32e76a392201e02b1c4eeb7
Pranav <pranavraj1720@gmail.com>
2021-12-07 17:00:30 +0530

    Completed Home Section

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Navbar Section Starts 
+
+let menu = document.querySelector("#menu-btn")
+let navbar = document.querySelector(".navbar")
+
+
+menu.onclick = () =>{
+    menu.classList.toggle("fa-times")
+    navbar.classList.toggle("active")
+}
+
+
+window.onscroll = () =>{
+    menu.classList.remove("fa-times")
+    navbar.classList.remove("active")
+}
+
+
+// Navbar Section Ends 
+
